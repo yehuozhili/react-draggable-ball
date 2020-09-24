deccb40891842cf9c817a402f6fc0799ed9d9a69
yehuozhili <34372936+yehuozhili@users.noreply.github.com>
2020-09-24 04:11:25 +0000

    Deploying to gh-pages from  @ d473a4b0c605d421c8acbe0eebc59532a9deaf78 🚀

---
+++ precache-manifest.c58bbbbf2531c0eeae6f9f9eab78374f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d7bda11b9c43d25922a4bf6a6144c1",
+    "url": "/react-draggable-ball/index.html"
+  },
+  {
+    "revision": "267b03809b267e4cf2fc",
+    "url": "/react-draggable-ball/static/js/2.f4d34c03.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-draggable-ball/static/js/2.f4d34c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57e393a85d307cd48dd",
+    "url": "/react-draggable-ball/static/js/main.0f525cd8.chunk.js"
+  },
+  {
+    "revision": "8c8aa98c3b8cd75fb716",
+    "url": "/react-draggable-ball/static/js/runtime-main.d9d4cb69.js"
+  }
+]);
